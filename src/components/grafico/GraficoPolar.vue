f1edd01c4a430f2c6bb2f99a9ed3b1d2735d9cc9
Antonio Carlos <baptistaacbc@hotmail.com>
2017-11-07 23:41:18 -0200

    Adicionada todos os graficos para Vue

---
+++ src/components/grafico/GraficoPolar.vue
@@ -0,0 +1,41 @@
+<script>
+var randomScalingFactor = function() {
+        return Math.round(Math.random() * 100);
+    };
+
+var chartColors = window.chartColors;
+var color = Chart.helpers.color;
+import { PolarArea } from 'vue-chartjs'
+  export default {
+    name: 'grafico-polar',
+    extends: PolarArea,
+      mounted () {
+        this.renderChart({
+            datasets: [{
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                ],
+                backgroundColor: [
+                    color('#C2185B').alpha(0.5).rgbString(),
+                    color('#757575').alpha(0.5).rgbString(),
+                    color('#03A9F4').alpha(0.5).rgbString(),
+                    color('#212121').alpha(0.5).rgbString(),
+                    color('#F8BBD0').alpha(0.5).rgbString(),
+                ],
+                label: 'My dataset' // for legend
+            }],
+            labels: [
+                "Red",
+                "Orange",
+                "Yellow",
+                "Green",
+                "Blue"
+            ]
+        }, {responsive: true, maintainAspectRatio: false})
+      }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 Vue.use(VueMaterial)
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#app1',
   router,
   template: '<App/>',
   components: { App }
