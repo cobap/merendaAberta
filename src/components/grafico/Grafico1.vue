a18b71fa21a8b437ddf0115c7cc6e06971bbc410
Rafael Bortman <rafaelbortman@yahoo.com.br>
2017-11-29 02:26:00 +0000

    2 graficos adicionados

---
+++ src/components/grafico/Grafico1.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios'
+import { Line } from 'vue-chartjs'
+export default {
+name: 'grafico',
+extends: Line,
+mounted () {
+  axios.get(`https://merendaabertaapi.herokuapp.com/api/v1/subprefeitura/`).then(response => {
+  const subPref = response.data
+
+// Após o .map a variável siglaSubPref
+// é um array com todas as siglas que estão
+// dentro do retorno da API
+  const siglaSubPref = subPref.map(el => {
+    return el.siglaSubPref
+  })
+
+  const idhm = subPref.map(el => {
+    return el.idhm
+  })
+
+  this.renderChart({
+    labels: siglaSubPref,
+    datasets: [
+    {
+      label: 'IDH médio',
+      backgroundColor: '#f87979',
+      data: idhm
+    }
+  ]
+  },{
+    responsive: true,
+    maintainAspectRatio: false
+  })
+}).catch(e => {
+  this.errors.push(e)
+})
+}
+}
+</script>
